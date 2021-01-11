3c99bc8893301ddc76ffe53477666bed13c16c6d
clilc <clilc@naver.com>
2021-01-11 21:20:39 +0900

    Add impl

--- support-portal/src/main/java/com/supportpotal/supportpotal/service/impl/UserServiceImpl.java
+++ support-portal/src/main/java/com/supportpotal/supportpotal/service/impl/UserServiceImpl.java
@@ -56,7 +56,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
             LOGGER.info(FOUND_USER_BY_USERNAME + username);
             return userPrincipal;
         }
-    } //check
+    } //check1
 
     private void validateLoginAttempt(User user) {
         if(user.isNotLocked()) {
